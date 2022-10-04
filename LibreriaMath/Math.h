248db98b7673ce4fa7ce2667fe43651a665066e7
Danthalian <a22110090@gmail.com>
2022-10-04 15:44:17 -0500

    V04

--- LibreriaMath/Math.h
+++ LibreriaMath/Math.h
@@ -8,44 +8,46 @@ public:
 	void Cos();
 	void Tan();
 	void Matriz();
-	int grados;
+	double grados;
+	double g;
 private:
-	int x;
-	int y;
-	int x2;
-	int y2;
-	int t;
+	long double seno; 
+	long double coseno;
+	long double temp;
+	long double tang;
+	int x;	int y;
+	int x2;	int y2;
 };
 
 Math::Math(){
-	grados = 0;
-	x = 0;
-	y = 0;
-	x2 = 0;
-	y2 = 0;
-	t = 0;
+	seno = 0; coseno = 0; tang = 0; temp = 0;
+	grados = 0; g = 0;
+	x = 0;	y = 0;	x2 = 0;	y2 = 0;
 }
 
 void Math::Sen(){
 	cout << "introduzca el angulo a calcular" << endl;
-	cin >> grados;
-	long double temp;
-	temp = ((4*grados*(180-grados)));
-	long double resultado;
-	resultado = temp / (40500 - (grados * (180 - grados)));
-	cout << "El seno de: "<<grados<<" es: "<<resultado<< endl;
+	cin >> grados;	
+	temp = ((4*grados*(180-grados)));	
+	seno = temp / (40500 - (grados * (180 - grados)));
+	cout << "El Seno de: "<<grados<<" es: "<<seno<< endl;
 }
 
 void Math::Cos() {
 	cout << "introduzca el angulo a calcular" << endl;
-	cin >> grados;
-	cout << "De momento jala 2" << endl;
+	cin >> g;
+	grados = 90 - g;
+	temp = ((4 * grados * (180 - grados)));
+	coseno = temp / (40500 - (grados * (180 - grados)));
+	cout << "El Coseno de: " << g << " es: " << coseno << endl;
 }
 
 void Math::Tan() {
 	cout << "introduzca el angulo a calcular" << endl;
 	cin >> grados;
-	cout << "De momento jala 3" << endl;
+	Sen();	Cos();
+	tang = seno / coseno;
+	cout << "La tangente de: " << grados << " es: " << tang << endl;
 }
 
 void Math::Matriz() {
@@ -63,38 +65,42 @@ void Math::Matriz() {
 
 	if (x >= 5 || y >= 5) {
 		cout << "El tamano maximo es de 5" << endl;
-	}else {
+	}else if(y==x2){
 		cout << "Introduzca los valores de la primera matriz: " << endl;
 		for (int i = 0; i < x; i++) {
 			for (int j = 0; j < y; j++) {
-				cout << "[" << i << "]" << "[" << j << "]" << endl;
+				cout << "M1 [" << i << "]" << "[" << j << "]: ";
 				cin >> matriz[i][j][0];
 			}
 		}
 		cout << "Introduzca los valores de la segunda matriz: " << endl;
 		for (int i = 0; i < x2; i++) {
 			for (int j = 0; j < y2; j++) {
-				cout << "[" << i << "]" << "[" << j << "]" << endl;
+				cout << "M2 [" << i << "]" << "[" << j << "]: ";
 				cin >> matriz[i][j][1];
 			}
 		}
 
-		for (int i = 0; i <= y; i++) {
-			for (int j = 0; j <= x2; j++) {
-				t = t + (matriz[i][j][0] * matriz[i][j][1]);
-				matriz[i][j][2] = t;
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y2; j++) {
+				matriz[i][j][2] = 0;
+				for(int k = 0; k< y; k++){
+					matriz[i][j][2] += matriz[i][k][0] * matriz[k][j][1];
+				}
 			}
-			
-			t = 0;
 		}
 		cout << "Valores de la matriz: " << endl;
-		for (int i = 0; i < y; i++) {
-			for (int j = 0; j < x2; j++) {
-				cout << "[" << i << "]" << "[" << j << "]= "<< matriz[i][j][2] << endl;
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y2; j++) {
+				cout << "[" << i << "]" << "[" << j << "]= " << matriz[i][j][2] << " ";
 			}
+			cout << endl;
 		}
 
+	}else {
+		cout << "El largo de las columnas de la M1 y las filas del M2 deben ser iguales" << endl;
 	}
+
 
 }
 
